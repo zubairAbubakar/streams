38b93e1b2ef252a562ebfed3ee96ef49ac21a525
Zubair Abubakar <zuby52@gmail.com>
2021-11-04 16:09:25 +0100

    implmented stream show page

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "actions";
 import StreamForm from "./StreamForm";
