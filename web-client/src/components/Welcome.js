382d22967c07d3b99d120080dd560f7a5f5f9f86
Zubair Abubakar <zuby52@gmail.com>
2024-02-15 22:21:19 +0000

    added new view component

---
+++ web-client/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header';
+
+const Welcome = () => {
+    return (
+        <div>
+            <Header />
+            App component!
+        </div>
+    );
+};
+
+export default Welcome;
